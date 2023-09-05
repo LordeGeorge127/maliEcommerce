514bb9a4aebdac92eaeb8221aba86316dcab9f5e
LordeGeorge <george.ken.127@gmail.com>
2023-09-05 14:29:47 +0300

    added firstname,lastname on signup and also added columns o user model

---
+++ frontend/controllers/ProfileController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace frontend\controllers;
+
+use yii\base\Controller;
+
+class ProfileController extends  Controller
+{
+    public function actionIndex()
+    {
+
+    }
+}
--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -36,8 +36,11 @@ AppAsset::register($this);
     ]);
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
+        ['label' => 'Cart <span id="cart-quantity" class="badge badge-danger" style="color:black">5</span> ',
+            'url' => ['/cart/index'],
+            'class'=>['btn btn-outline-dark mt-auto'],
+            'encode'=>false],
+
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
